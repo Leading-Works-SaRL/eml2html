dd7499fb9de7b6ef4bb2d4e046b4bf29ffbfc761
soriyath <soriyath@leading.works>
2021-07-16 09:48:16 +0200

    Implement eml2html simple converter

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const EmlParser = require('eml-parser');
+const clipboardy = require('clipboardy');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,26 +9,27 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "eml2html" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('eml2html.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from eml2html!');
+	let disposable = vscode.commands.registerCommand('eml2html.convert', function () {
+		eml2Html();
+		vscode.window.showInformationMessage('EML was converted to HTML and saved in the clipboard.');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
+
+function eml2Html() {
+	if (!vscode.window.activeTextEditor) {
+		return;
+	}
+
+	const eml = clipboardy.readSync();
+	new EmlParser(eml)
+		.getEmailBodyHtml()
+		.then(html => clipboardy.writeSync(html))
+		.catch(err => console.error(err));
+}
 
 module.exports = {
 	activate,
